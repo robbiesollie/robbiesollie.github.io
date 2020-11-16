5b407b87239210186c03ce3431d0a10bae6773cb
Robbie <robbiesollie@gmail.com>
2020-11-15 16:05:22 -0800

    :(

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+<script>
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'index.css';
+</script>
 
 function Square(props) {
   return (
