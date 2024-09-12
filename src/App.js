609dcfe2b8eb1d85064974c4975ad2a27cfce0eb
robbiesollie <robbiesollie@gmail.com>
2024-09-12 13:22:31 -0700

    lets figure out how to make this react now

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js'
+import Home from './Home.js'
+
+function App() {
+  return (<>
+    <Header />
+    <Home />
+  </>);
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<div className="header">
+			<div className="left">
+				<a href="/">Home</a>
+				<a href="https://www.example.com">Blog</a>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
